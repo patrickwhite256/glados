0ec183ab66e055c0252db19167dd4a690285957e
Patrick White <patrickwhite.256@gmail.com>
2015-07-30 23:45:43 -0700

    One of these days I'll fix everything

--- plugins/gifme.py
+++ plugins/gifme.py
@@ -21,16 +21,16 @@ class GifMe(GladosPluginBase):
         return query_re.match(msg['text'])
 
     def handle_message(self, msg):
-        query_match = query_re.match(msg['text'])
+        query_str = query_re.match(msg['text']).group(1)
 
-        query_url = GIPHY_API_URL_TPL.format(query=query_match.group(1))
+        query_url = GIPHY_API_URL_TPL.format(query=query_str)
 
         r = requests.get(query_url)
 
         def send_fail_msg(err=None):
             if err:
                 print(err)
-            self.send('No results found for "{}"'.format(query_match), msg['channel'])
+            self.send('No results found for "{}"'.format(query_str), msg['channel'])
 
         if r.status_code != requests.codes.ok:
             send_fail_msg()
