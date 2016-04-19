04fcbbe0c7c50bdf8585280b86427661bbf26035
Nanyi Jiang <leonjiang@fb.com>
2016-04-18 22:43:45 -0700

    React :triggergif: when message contains 'PTSD' or 'trigger(ed)'

--- plugins/lmao.py
+++ plugins/lmao.py
@@ -8,14 +8,23 @@ from plugin_base import GladosPluginBase
 class AyyLmao(GladosPluginBase):
     consumes_message = True
 
+    REGEX_TO_REACTION = {
+        r'\bay+(\b|lmao)': 'ayylmao',
+        r'\btrigger(\b|ed)|\bPTSD': 'triggergif',
+    }
+
     def can_handle_message(self, msg):
         if msg['type'] != 'message' or 'message' in msg:
             return None
-        return re.search(r'\bay+(\b|lmao)',
-                         msg['text'], flags=re.I) is not None
+        for regex in self.REGEX_TO_REACTION:
+            if re.search(regex, msg['text'], flags=re.I) is not None:
+                return True
+        return False
 
     def handle_message(self, msg):
-        self.react_to_message(msg, 'ayylmao')
+        for regex, reaction in self.REGEX_TO_REACTION.items():
+            if re.search(regex, msg['text'], flags=re.I) is not None:
+                self.react_to_message(msg, reaction)
 
     @property
     def help_text(self):
